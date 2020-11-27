dc09625e953404d47d4c30f65b75a601517602bd
Clayton Franklin <clayton.j.franklin@nevada.unr.edu>
2020-11-26 20:59:20 -0800

    Adding initial light configuration files

---
+++ arduino_lights/lights.ino
@@ -0,0 +1,22 @@
+/*
+File: lights.ino
+Author: Clayton Franklin
+Date: 26-Nov-2020
+Version: 1.0
+Purpose: Code to run Christmas lights for Christmas 2020
+*/
+#include <Adafruit_NeoPixel.h>
+
+// Setup for the main loop
+void setup() 
+{
+
+}
+
+// Run the code forever
+void loop() 
+{
+
+}
+
+// Functions for the lights
