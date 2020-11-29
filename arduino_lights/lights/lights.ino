257b43f2498a543d36b4b919ee1c59ade7c5b3a4
Clayton Franklin <clayton.j.franklin@nevada.unr.edu>
2020-11-28 19:27:51 -0800

    change amount of lights and add functions

--- arduino_lights/lights/lights.ino
+++ arduino_lights/lights/lights.ino
@@ -13,7 +13,7 @@ Purpose: Code to run Christmas lights for Christmas 2020
 
 // Define number of LEDs for each strand
 #define UPPER_LED_COUNT     450
-#define LOWER_LED_COUNT     150
+#define LOWER_LED_COUNT     487
 
 // Define LED brightness
 #define LED_BRIGHTNESS      192
@@ -33,6 +33,16 @@ Argument Definition:
         NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 *************************************************/
 
+// Array for pre-defined colors
+const uint32_t colors[5] = 
+{
+    upperStrand.Color(255,  0,      0),     // Red
+    upperStrand.Color(0,    255,    0),     // Blue
+    upperStrand.Color(0,    0,      255),   // Green
+    upperStrand.Color(255,  165,    0),     // Orange
+    upperStrand.Color(127,  127,    127)    // White (Half Brightness)
+};
+
 // Setup for the main loop
 void setup() 
 {
@@ -44,15 +54,18 @@ void setup()
     lowerStrand.show();
     // Set brightness of LEDs on strands
     upperStrand.setBrightness(LED_BRIGHTNESS);
+
+    // Define an array of colors
 }
 
 // Run the code forever
 void loop() 
 {
-    colorWipe(upperStrand.Color(255, 0, 0), 10);
-    //twinkleLights(upperStrand.Color(0, 0 , 255), 50, 100, false);
-    upperStrand.clear();
-    lowerStrand.clear();
+    //colorWipe(upperStrand.Color(255, 0, 0), 10);
+    twinkleLights(colors, 10, LOWER_LED_COUNT, false);
+    clear();
+    runningLights(127, 127, 127, 1);
+    clear();
 }
 
 // Functions for the lights
@@ -64,7 +77,7 @@ Argument Definition:
 *************************************************/
 void colorWipe(uint32_t color, int wait) 
 {
-    for (int i = 0; i < upperStrand.numPixels(); i++)
+    for (int i = 0; i < lowerStrand.numPixels(); i++)
     {
         upperStrand.setPixelColor(i, color);
         lowerStrand.setPixelColor(i, color);
@@ -82,14 +95,14 @@ Argument Definition:
     Arg 3: number of LEDs to light up in one round
     Arg 4: only see one LED at a time
 *************************************************/
-void twinkleLights(uint32_t color, int wait, int number, bool onlyOne)
+void twinkleLights(const uint32_t* color, int wait, int number, bool onlyOne)
 {
     upperStrand.clear();
     lowerStrand.clear();
     for (int i = 0; i < number; i++)
     {
-        upperStrand.setPixelColor(random(UPPER_LED_COUNT), color);
-        lowerStrand.setPixelColor(random(LOWER_LED_COUNT), color);
+        upperStrand.setPixelColor(random(UPPER_LED_COUNT), color[i % 5]);
+        lowerStrand.setPixelColor(random(LOWER_LED_COUNT), color[i % 5]);
         upperStrand.show();
         lowerStrand.show();
         delay(wait);
@@ -113,13 +126,57 @@ void runningLights(uint8_t red, uint8_t green, uint8_t blue, int wait)
 {
     int position = 0;
 
-    for (int j = 0; j < UPPER_LED_COUNT * 2; j++)
+    for (int j = 0; j < 100; j++)
     {
         position++;
-        for (int i = 0; i < UPPER_LED_COUNT; i++)
+        for (int i = 0; i < LOWER_LED_COUNT; i++)
         {
-            uint8_t redCalc = 0;
-            //upperStrand.setPixelColor(i, upperStrand.Color());
+            uint8_t redCalc = ((sin(i + position) * 127 + 128) / 255) * red;
+            uint8_t greenCalc = ((sin(i + position) * 127 + 128) / 255) * green;
+            uint8_t blueCalc = ((sin(i + position) * 127 + 128) / 255) * blue;
+            upperStrand.setPixelColor(i, upperStrand.Color(redCalc, greenCalc, blueCalc));
+            lowerStrand.setPixelColor(i, lowerStrand.Color(redCalc, greenCalc, blueCalc));
+        }
+        upperStrand.show();
+        lowerStrand.show();
+        delay(wait);
+    }
+}
+
+/*************************************************
+Function theaterChase(color, wait)
+Argument Definition:
+    Arg 1: Color value for the strip
+    Arg 2: wait time for marquee
+*************************************************/
+void theaterChase(uint32_t color, int wait) 
+{   
+    // Repeat 10 times...
+    for(int a = 0; a < 10; a++) 
+    {
+        // 'b' counts from 0 to 2...
+        for(int b = 0; b < 3; b++) 
+        { 
+            clear();  //   Set all pixels in RAM to 0 (off)
+
+            // 'c' counts up from 'b' to end of strip in steps of 3...
+            for(int c = b; c < lowerStrand.numPixels(); c += 3) 
+            {
+                upperStrand.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+                lowerStrand.setPixelColor(c, color);
+            }
+
+            upperStrand.show(); // Update strip with new contents
+            lowerStrand.show();
+            delay(wait);  // Pause for a moment
         }
     }
+}
+
+// Helper Functions
+
+void clear()
+{
+    upperStrand.clear();
+    lowerStrand.clear();
 }
