6e12299b4e25ec65a7d76df494745b8a9850398d
Clayton Franklin <clayton.j.franklin@nevada.unr.edu>
2020-12-05 12:29:28 -0800

    fixing timing for auto turn off

--- scripts/arm.py
+++ scripts/arm.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 from gpiozero import LED
-from datetime import datetime, timedelta
+from time import sleep
 
 # Put the relay on RPi Pin 23
 relay = LED(23)
-# Set the time to turn off 5 hours from now
-endTime = datetime.now() + timedelta(hours=5)
-# Loop until 5 hours has passed
-while True:
-    # Check if the time elapsed has been 5 hours
-    if datetime.now() >= endTime:
-        # Turn off the relay
-        relay.off()
-        break
-    # Keep the relay on
-    relay.on()
+# Turn the relay on
+relay.on()
+# Sleep for 5 hours
+sleep(18000)
+# Turn the relay off
+relay.off()
